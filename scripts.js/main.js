d218987864471618c2efc5a38f892daa983d34bc
Pedro Eyer <pedrojulioeyr@gmail.com>
2025-05-15 18:05:05 -0300

    .

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -48,3 +48,28 @@ document.addEventListener("DOMContentLoaded", () => {
         overlay2.style.display = "none";
     });
 });
+// Destaque na seção ativa
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("#main-nav ul li a");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").includes(current)) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
+// esconder o conteudo 
